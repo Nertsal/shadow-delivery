e62c210e6c70ddf0df8761e7dd31fa9ca05a2fac
Nertsal <sasha.kudasov04@gmail.com>
2023-04-29 16:28:47 +0300

    Setup geng

---
+++ src/game/mod.rs
@@ -0,0 +1,21 @@
+use super::*;
+
+pub struct Game {
+    geng: Geng,
+    assets: Rc<Assets>,
+}
+
+impl Game {
+    pub fn new(geng: &Geng, assets: &Rc<Assets>) -> Self {
+        Self {
+            geng: geng.clone(),
+            assets: assets.clone(),
+        }
+    }
+}
+
+impl geng::State for Game {
+    fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        ugli::clear(framebuffer, Some(Rgba::BLACK), None, None);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use ecs::prelude::*;
+use geng::prelude::*;
+
+mod assets;
+mod game;
+
+use assets::Assets;
+
 fn main() {
-    println!("Hello, world!");
+    let geng = Geng::new_with(geng::ContextOptions {
+        title: "Shadow Delivery".to_string(),
+        ..default()
+    });
+
+    let future = {
+        let geng = geng.clone();
+        async move {
+            let assets: Assets = geng::Load::load(geng.asset_manager(), &run_dir().join("assets"))
+                .await
+                .expect("Failed to load assets");
+            game::Game::new(&geng, &Rc::new(assets))
+        }
+    };
+
+    geng.run_loading(future)
 }
