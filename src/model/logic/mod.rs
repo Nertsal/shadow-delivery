bd3ebef6a99ff9b1439af8553bf0d08c7e11da6b
Nertsal <sasha.kudasov04@gmail.com>
2023-04-29 21:30:13 +0300

    Collisions

--- src/model/logic/mod.rs
+++ src/model/logic/mod.rs
@@ -8,6 +8,7 @@ impl World {
     pub fn update(&mut self, player_control: PlayerControl, delta_time: Time) {
         self.control_player(player_control, delta_time);
         self.movement(delta_time);
+        self.collisions();
     }
 
     fn control_player(&mut self, control: PlayerControl, delta_time: Time) {
@@ -21,5 +22,23 @@ impl World {
         let dir = vec2::UNIT_X.rotate(self.player.collider.rotation);
         let delta = dir * self.player.speed * delta_time;
         self.player.collider.translate(delta);
+    }
+
+    fn collisions(&mut self) {
+        #[derive(StructQuery)]
+        struct ObstacleRef<'a> {
+            collider: &'a Collider,
+        }
+
+        let player = &mut self.player;
+
+        for obstacle in query_obstacle_ref!(self.obstacles).values() {
+            if let Some(collision) = player.collider.collide(obstacle.collider) {
+                player
+                    .collider
+                    .translate(-collision.normal * collision.penetration);
+                // TODO: fix velocity
+            }
+        }
     }
 }
