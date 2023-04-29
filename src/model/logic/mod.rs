e23c9d2b7ffe1e16d684742f07a9392aaa1122ed
Nertsal <sasha.kudasov04@gmail.com>
2023-04-29 21:42:33 +0300

    Player drag

--- src/model/logic/mod.rs
+++ src/model/logic/mod.rs
@@ -1,8 +1,9 @@
 use super::*;
 
+const PLAYER_DRAG: f32 = 0.1;
+const PLAYER_MAX_SPEED: f32 = 5.0;
 const PLAYER_TURN_SPEED: f32 = 3.0;
 const PLAYER_ACCELERATION: f32 = 10.0;
-const PLAYER_MAX_SPEED: f32 = 5.0;
 
 impl World {
     pub fn update(&mut self, player_control: PlayerControl, delta_time: Time) {
@@ -13,6 +14,7 @@ impl World {
 
     fn control_player(&mut self, control: PlayerControl, delta_time: Time) {
         self.player.collider.rotation += control.turn * Coord::new(PLAYER_TURN_SPEED) * delta_time;
+        self.player.speed -= self.player.speed * Coord::new(1.0 - PLAYER_DRAG) * delta_time;
         let target_speed =
             self.player.speed + control.accelerate * Coord::new(PLAYER_ACCELERATION) * delta_time;
         self.player.speed = target_speed.clamp(Coord::ZERO, Coord::new(PLAYER_MAX_SPEED));
